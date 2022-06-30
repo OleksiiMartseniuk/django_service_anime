094ba5086a3d1960103541735b3d7ee0cd2ddab7
martsenyk.l@gmail.com <6n8at3TUS7>
2022-06-30 13:22:09 +0300

    add app anime and drf_yasg

--- config/settings.py
+++ config/settings.py
@@ -28,6 +28,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'drf_yasg',
+
+    'src.anime',
 ]
 
 MIDDLEWARE = [
--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,35 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Anime API",
+        default_version='v1',
+        description="Anime",
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
 
 urlpatterns = [
+    # admin django
     path('admin/', admin.site.urls),
+    # drf_yasg
+    path(
+        'swagger/',
+        schema_view.with_ui('swagger', cache_timeout=0),
+        name='schema-swagger-ui'
+    ),
+    path(
+        'redoc/',
+        schema_view.with_ui('redoc', cache_timeout=0),
+        name='schema-redoc'
+    ),
+    # app anime
+    path('v1/anime/', include('src.anime.urls')),
 ]
