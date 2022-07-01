23b8687af1f54f9f5b06f1af393fc2299d3714c9
martsenyk.l@gmail.com <6n8at3TUS7>
2022-07-01 11:11:58 +0300

    add max len

--- src/anime/models.py
+++ src/anime/models.py
@@ -11,14 +11,14 @@ class Genre(models.Model):
 
 class ScreenImages(models.Model):
     """Набор кадров"""
-    images = models.CharField('Скрин')
+    images = models.CharField('Скрин', max_length=255)
 
 
 class Series(models.Model):
     """Серии"""
     name = models.CharField('Номер серии', max_length=50)
-    std = models.CharField('sd качество')
-    hd = models.CharField('hd качество')
+    std = models.CharField('sd качество', max_length=255)
+    hd = models.CharField('hd качество', max_length=255)
 
     def __str__(self):
         return self.name
@@ -27,16 +27,16 @@ class Series(models.Model):
 class Anime(models.Model):
     """Аниме"""
     id_anime = models.IntegerField('ID animevost', unique=True)
-    title = models.CharField('Названия')
-    link = models.CharField('Ссылка animevost')
+    title = models.CharField('Названия', max_length=255)
+    link = models.CharField('Ссылка animevost', max_length=255)
     screen_image = models.ManyToManyField(Series, related_name='screen_images')
     genre = models.ManyToManyField(Genre, related_name='genres')
     rating = models.IntegerField('Рейтинг')
     votes = models.IntegerField('Голоса')
     description = models.TextField('Описания')
     director = models.CharField('Режиссёр', max_length=100)
-    url_image_preview = models.CharField('Preview изображения')
-    year = models.CharField('Год выпуска')
+    url_image_preview = models.CharField('Preview изображения', max_length=255)
+    year = models.CharField('Год выпуска', max_length=20)
     timer = models.IntegerField('Время выхода серии', default=0)
     type = models.CharField('Тип', max_length=10)
     day_week = models.CharField('День недели', max_length=20, default=None)
