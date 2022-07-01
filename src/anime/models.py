f17568d4a3afbf0b1ab7defe0f056d7c17b73c44
martsenyk.l@gmail.com <6n8at3TUS7>
2022-07-01 15:55:40 +0300

    add unique True

--- src/anime/models.py
+++ src/anime/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Genre(models.Model):
     """Жанры"""
-    title = models.CharField('Названия', max_length=30)
+    title = models.CharField('Названия', unique=True, max_length=30)
 
     def __str__(self):
         return self.title
