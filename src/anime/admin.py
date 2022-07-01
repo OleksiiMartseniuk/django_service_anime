1ecd38be6995af7b78d5c9d93eaf5d1817c46940
martsenyk.l@gmail.com <6n8at3TUS7>
2022-07-01 10:56:20 +0300

    add register admin

--- src/anime/admin.py
+++ src/anime/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import Anime, Genre, Series, ScreenImages
 
-# Register your models here.
+
+@admin.register(Anime)
+class AnimeAdmin(admin.ModelAdmin):
+    list_display = (
+        'title', 'id_anime', 'rating', 'votes', 'day_week', 'anons'
+    )
+    list_filter = ('day_week', 'anons', 'rating', 'votes')
+
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+
+
+@admin.register(Series)
+class SeriesAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    list_filter = ('name',)
+
+
+@admin.register(ScreenImages)
+class ScreenImagesAdmin(admin.ModelAdmin):
+    list_display = ('images',)
