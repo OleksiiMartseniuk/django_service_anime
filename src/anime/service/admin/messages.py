1085acf6d9086b0ecbf114adb2ceb24fd2110703
martsenyk.l@gmail.com <6n8at3TUS7>
2022-07-05 19:54:01 +0300

    add message info

---
+++ src/anime/service/admin/messages.py
@@ -0,0 +1,10 @@
+SCHEDULE = 'Parser запущен идет сбор данных...'
+ANONS = 'Parser запущен идет сбор данных...'
+DElETE = 'Данные удалены'
+ERROR = 'Сбой в роботе!'
+ERROR_STATUS_CODE_PARSE = 'ParserClientStatusCodeError\n' \
+                          'Неверный статус код ответа'
+ERROR_STATUS_CODE_API = 'ApiAnimeVostClientStatusCodeError\n' \
+                        'Неверный статус код ответа'
+ERROR_DATA_API = 'ApiAnimeVostClientAttributeError\n' \
+                 'Пришедшие дынные с api animevost не верны'
