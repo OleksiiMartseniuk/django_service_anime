313aa8a4ec2d1faeee3c33ba2c0538ec9bda98f0
martsenyk.l@gmail.com <6n8at3TUS7>
2022-07-05 18:22:07 +0300

    add setting parser

--- src/anime/admin.py
+++ src/anime/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
+from django.template.response import TemplateResponse
+from django.urls import path
+
 from .models import Anime, Genre, Series, ScreenImages
+from .forms import ParserForm
 
 
 @admin.register(Anime)
@@ -9,6 +13,27 @@ class AnimeAdmin(admin.ModelAdmin):
     )
     list_filter = ('day_week', 'anons')
     search_fields = ('title', 'id_anime')
+
+    def get_urls(self):
+        urls = super().get_urls()
+        my_urls = [
+            path('parser/', self.admin_site.admin_view(self.parser))
+        ]
+        return my_urls + urls
+
+    def parser(self, request):
+        form = ParserForm()
+
+        if request.method == 'POST':
+            form = ParserForm(request.POST)
+            if form.is_valid():
+                pass
+
+        context = dict(
+            self.admin_site.each_context(request),
+            form=form
+        )
+        return TemplateResponse(request, 'admin/parser.html', context)
 
 
 @admin.register(Genre)
