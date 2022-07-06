639d6d1afabb7f76594b79b13b32058801d722a6
martsenyk.l@gmail.com <6n8at3TUS7>
2022-07-06 15:02:51 +0300

    add exception admin write

--- src/anime/service/admin/service.py
+++ src/anime/service/admin/service.py
@@ -1,6 +1,8 @@
 from dataclasses import dataclass
 from django.contrib import messages
 
+from src.base.animevost import exception
+
 from ..service import ServiceAnime
 from . import messages as ex_massages
 
@@ -15,19 +17,46 @@ class Status:
 class ParserControl:
     """Контролер Parser"""
     def control(self, action: str) -> Status:
-        match action:
-            case 'schedule':
-                ServiceAnime().anime_schedule()
-                status = Status(message=ex_massages.SCHEDULE)
-            case 'anons':
-                ServiceAnime().anime_anons()
-                status = Status(message=ex_massages.ANONS)
-            case 'delete':
-                ServiceAnime().delete_table()
-                status = Status(message=ex_massages.DElETE)
-            case _:
-                status = Status(
-                    message=ex_massages.ERROR,
-                    level=messages.ERROR
-                )
+        try:
+            match action:
+                case 'schedule':
+                    ServiceAnime().anime_schedule()
+                    status = Status(message=ex_massages.SCHEDULE)
+                case 'anons':
+                    ServiceAnime().anime_anons()
+                    status = Status(message=ex_massages.ANONS)
+                case 'delete':
+                    ServiceAnime().delete_table()
+                    status = Status(message=ex_massages.DElETE)
+                case _:
+                    status = Status(
+                        message=ex_massages.ERROR,
+                        level=messages.ERROR
+                    )
+        except exception.ParserClientStatusCodeError:
+            status = Status(
+                message=ex_massages.ERROR_STATUS_CODE_PARSE,
+                level=messages.ERROR
+            )
+        except exception.ApiAnimeVostClientStatusCodeError:
+            status = Status(
+                message=ex_massages.ERROR_STATUS_CODE_API,
+                level=messages.ERROR
+            )
+        except exception.ApiAnimeVostClientAttributeError:
+            status = Status(
+                message=ex_massages.ERROR_DATA_API,
+                level=messages.ERROR
+            )
+        except exception.NotDataError:
+            status = Status(
+                message=ex_massages.ERROR_NOT_DATA,
+                level=messages.ERROR
+            )
+        except Exception as ex:
+            status = Status(
+                message=ex_massages.ERROR,
+                level=messages.ERROR
+            )
+
         return status
