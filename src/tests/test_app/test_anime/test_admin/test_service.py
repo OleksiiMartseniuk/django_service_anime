3bb2eabc450ab846eb1efc67d30238ec37c71027
martsenyk.l@gmail.com <6n8at3TUS7>
2022-07-10 19:51:37 +0300

    edit test

--- src/tests/test_app/test_anime/test_admin/test_service.py
+++ src/tests/test_app/test_anime/test_admin/test_service.py
@@ -8,24 +8,24 @@ from src.anime.service.admin import messages
 
 
 class TestParserControl(APITestCase):
-    @mock.patch('src.anime.service.admin.service.ServiceAnime.anime_schedule')
-    def test_control_schedule(self, mock_anime_schedule):
+    @mock.patch('src.anime.service.admin.service.parser.delay')
+    def test_control_schedule(self, mock_parser):
         message = ParserControl().control('schedule')
-        mock_anime_schedule.assert_called_once()
+        mock_parser.assert_called_once()
 
         self.assertEqual(message, Status(messages.SCHEDULE))
 
-    @mock.patch('src.anime.service.admin.service.ServiceAnime.anime_anons')
-    def test_control_anons(self, mock_anime_anons):
+    @mock.patch('src.anime.service.admin.service.parser.delay')
+    def test_control_anons(self, mock_parser):
         message = ParserControl().control('anons')
-        mock_anime_anons.assert_called_once()
+        mock_parser.assert_called_once()
 
         self.assertEqual(message, Status(messages.ANONS))
 
-    @mock.patch('src.anime.service.admin.service.ServiceAnime.delete_table')
-    def test_control_delete(self, mock_delete_table):
+    @mock.patch('src.anime.service.admin.service.parser.delay')
+    def test_control_delete(self, mock_parser):
         message = ParserControl().control('delete')
-        mock_delete_table.assert_called_once()
+        mock_parser.assert_called_once()
 
         self.assertEqual(message, Status(messages.DElETE))
 
@@ -34,20 +34,16 @@ class TestParserControl(APITestCase):
 
         self.assertEqual(message, Status(messages.ERROR, dj_messages.ERROR))
 
-    @mock.patch(
-        'src.anime.service.admin.service.ServiceAnime.anime_schedule_update'
-    )
-    def test_control_schedule_update(self, mock_anime_schedule_update):
+    @mock.patch('src.anime.service.admin.service.parser.delay')
+    def test_control_schedule_update(self, mock_parser):
         message = ParserControl().control('schedule_update')
-        mock_anime_schedule_update.assert_called_once()
+        mock_parser.assert_called_once()
 
         self.assertEqual(message, Status(messages.SCHEDULE_UPDATE))
 
-    @mock.patch(
-        'src.anime.service.admin.service.ServiceAnime.anime_anons_update'
-    )
-    def test_control_anons_update(self, mock_anime_anons_update):
+    @mock.patch('src.anime.service.admin.service.parser.delay')
+    def test_control_anons_update(self, mock_parser):
         message = ParserControl().control('anons_update')
-        mock_anime_anons_update.assert_called_once()
+        mock_parser.assert_called_once()
 
         self.assertEqual(message, Status(messages.ANONS_UPDATE))
