6f471fd8039b255d8a3421f3de6d9d2d0d0d21d1
martsenyk.l@gmail.com <6n8at3TUS7>
2022-07-13 17:28:25 +0300

    add function get_anime_list_day

---
+++ src/anime/service/service.py
@@ -0,0 +1,19 @@
+from typing import List
+
+from rest_framework.exceptions import ValidationError
+
+from src.anime.models import Anime
+
+
+WEEK = ['monday', 'tuesday', 'wednesday',
+        'thursday', 'friday', 'saturday', 'sunday']
+
+
+def get_anime_list_day(day: str) -> List[dict]:
+    """Получения списка аниме по дню недели"""
+    if day not in WEEK:
+        raise ValidationError(f'Неверное значения[{day}]-[{", ".join(WEEK)}]')
+    anime_list = Anime.objects. \
+        filter(day_week=day). \
+        values('id', 'title', 'url_image_preview')
+    return anime_list
