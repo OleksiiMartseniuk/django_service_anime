9de8047d2852cebed57c89ae448c625ef7112beb
martsenyk.l@gmail.com <6n8at3TUS7>
2022-07-13 13:14:35 +0300

    add path AnimeAnonsListView and AnimeDetailView

--- src/anime/urls.py
+++ src/anime/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
-
+    path('anime/anons/', views.AnimeAnonsListView.as_view()),
+    path('anime/<int:pk>/', views.AnimeDetailView.as_view()),
 ]
