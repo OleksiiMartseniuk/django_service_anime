2c19e76bf053a1c46828645cfc7952a97d7b691d
martsenyk.l@gmail.com <6n8at3TUS7>
2022-07-13 17:27:18 +0300

    add AnimeScheduleDayView

--- src/anime/views.py
+++ src/anime/views.py
@@ -1,7 +1,15 @@
-from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import generics, status
 
 from .models import Anime
-from .serializers import AnimeSerializers, AnimeMinAnonsSerializers
+from .service import service
+from .serializers import (
+    AnimeSerializers,
+    AnimeMinAnonsSerializers,
+    DaySerializers,
+    AnimeMinSerializers
+)
 
 
 class AnimeAnonsListView(generics.ListAPIView):
@@ -15,3 +23,20 @@ class AnimeDetailView(generics.RetrieveAPIView):
     """Вывод аниме"""
     queryset = Anime.objects.all()
     serializer_class = AnimeSerializers
+
+
+class AnimeScheduleDayView(APIView):
+    """Вывод расписания по дню"""
+    def post(self, request):
+        serializer_day = DaySerializers(data=request.data)
+        if serializer_day.is_valid():
+            anime_list = service.get_anime_list_day(serializer_day.data['day'])
+            serializer_anime = AnimeMinSerializers(anime_list, many=True)
+            return Response(
+                serializer_anime.data,
+                status=status.HTTP_200_OK
+            )
+        return Response(
+            serializer_day.errors,
+            status=status.HTTP_400_BAD_REQUEST
+        )
