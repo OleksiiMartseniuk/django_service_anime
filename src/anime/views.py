bc0925a50e402c292fb8fc03640bbd785ee46096
martsenyk.l@gmail.com <6n8at3TUS7>
2022-07-13 13:14:16 +0300

    add AnimeAnonsListView and AnimeDetailView

--- src/anime/views.py
+++ src/anime/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from .models import Anime
+from .serializers import AnimeSerializers, AnimeMinAnonsSerializers
+
+
+class AnimeAnonsListView(generics.ListAPIView):
+    """Вывод списка аниме анонсов"""
+    queryset = Anime.objects.filter(anons=True).values('id', 'title',
+                                                       'url_image_preview')
+    serializer_class = AnimeMinAnonsSerializers
+
+
+class AnimeDetailView(generics.RetrieveAPIView):
+    """Вывод аниме"""
+    queryset = Anime.objects.all()
+    serializer_class = AnimeSerializers
