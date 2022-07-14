82d8d44ac2380f441164da930b18f903318b8738
martsenyk.l@gmail.com <6n8at3TUS7>
2022-07-14 23:12:07 +0300

    add fild form

--- src/anime/forms.py
+++ src/anime/forms.py
@@ -1,14 +1,17 @@
 from django import forms
-from .models import Anime
+from .models import Anime, Series
 
 
 class ParserForm(forms.Form):
     action = forms.ChoiceField(label='', widget=forms.Select, choices=[
         ('schedule', 'Запись аниме расписания'),
         ('anons', 'Запись аниме анонсов'),
-        ('delete', 'Удаления всех записей и кеша'),
+        ('delete', 'Удаления всех записей таблиц (Anime, ScreenImages, Genre)'),
         ('schedule_update', 'Обновить аниме расписания'),
         ('anons_update', 'Обновить аниме анонсов'),
+        ('series', 'Запись серий'),
+        ('series_update', 'Обновления серий'),
+        ('delete_series', 'Очистка данных таблицы Series'),
     ])
 
     def clean_action(self):
@@ -24,6 +27,12 @@ class ParserForm(forms.Form):
                 self.schedule_update_valid()
             case 'anons_update':
                 self.anons_update_valid()
+            case 'series':
+                self.schedule_valid()
+            case 'series_update':
+                self.series_update_valid()
+            case 'delete_series':
+                self.delete_series_valid()
         return action
 
     def schedule_valid(self):
@@ -45,3 +54,15 @@ class ParserForm(forms.Form):
     def anons_update_valid(self):
         if not Anime.objects.filter(anons=True).count():
             raise forms.ValidationError('Нет Данных для обновления!')
+
+    def series_valid(self):
+        if Series.objects.count() >= 1:
+            raise forms.ValidationError('Данные серий уже записаны!')
+
+    def series_update_valid(self):
+        if not Series.objects.count():
+            raise forms.ValidationError('Данных для обновления нет!')
+
+    def delete_series_valid(self):
+        if not Series.objects.count():
+            raise forms.ValidationError('Данных для удаления нет!')
