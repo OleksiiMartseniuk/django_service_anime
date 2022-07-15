6e8435c3d4629918b7f17c14d60e79adeade5ccd
martsenyk.l@gmail.com <6n8at3TUS7>
2022-07-15 18:21:56 +0300

    add AnimeSeriesListView

--- src/anime/views.py
+++ src/anime/views.py
@@ -1,34 +1,48 @@
+from django.utils.decorators import method_decorator
+
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import generics, status
 
+from drf_yasg import openapi
 from drf_yasg.utils import swagger_auto_schema
 
-from .models import Anime
+from .service.pagination import LargeResultsSetPagination
+from .models import Anime, Series
 from .service import service
 from .serializers import (
     AnimeSerializers,
     AnimeMinAnonsSerializers,
     DaySerializers,
-    AnimeMinSerializers
+    AnimeMinSerializers,
+    SeriesSerializers
 )
 
 
 class AnimeAnonsListView(generics.ListAPIView):
-    """Вывод списка аниме анонсов"""
+    """
+    Вывод списка аниме анонсов
+    ---
+    """
     queryset = Anime.objects.filter(anons=True).values('id', 'title',
                                                        'url_image_preview')
     serializer_class = AnimeMinAnonsSerializers
 
 
 class AnimeDetailView(generics.RetrieveAPIView):
-    """Вывод аниме"""
+    """
+    Вывод аниме
+    ---
+    """
     queryset = Anime.objects.all()
     serializer_class = AnimeSerializers
 
 
 class AnimeScheduleDayView(APIView):
-    """Вывод расписания по дню"""
+    """
+    Вывод расписания по дню
+    ---
+    """
     @swagger_auto_schema(request_body=DaySerializers,
                          responses={200: AnimeMinSerializers(many=True)})
     def post(self, request):
@@ -44,3 +58,30 @@ class AnimeScheduleDayView(APIView):
             serializer_day.errors,
             status=status.HTTP_400_BAD_REQUEST
         )
+
+
+@method_decorator(
+    name='get',
+    decorator=swagger_auto_schema(
+        manual_parameters=[
+            openapi.Parameter(
+                'id_anime',
+                openapi.IN_PATH,
+                type=openapi.TYPE_INTEGER
+            )
+        ]
+    ),
+)
+class AnimeSeriesListView(generics.ListAPIView):
+    """
+    Вывод серий по id_anime
+    ---
+    """
+    serializer_class = SeriesSerializers
+    pagination_class = LargeResultsSetPagination
+
+    def get_queryset(self):
+        id_anime = self.kwargs['id_anime']
+        queryset = Series.objects.filter(id_anime=id_anime).\
+            values('id', 'name', 'std', 'hd', 'number').order_by('number')
+        return queryset
