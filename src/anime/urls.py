1a82b06bdd40732d0445fefc202699bcd31fbf9b
martsenyk.l@gmail.com <6n8at3TUS7>
2022-07-15 18:22:08 +0300

    add path AnimeSeriesListView

--- src/anime/urls.py
+++ src/anime/urls.py
@@ -2,11 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path(
-        'anime/schedule-day/',
-         views.AnimeScheduleDayView.as_view(),
-         name='schedule-day'
-    ),
+    path('anime/schedule-day/', views.AnimeScheduleDayView.as_view(),
+         name='schedule-day'),
+    path('anime/series/<int:id_anime>/', views.AnimeSeriesListView.as_view(),
+         name='series'),
     path('anime/anons/', views.AnimeAnonsListView.as_view(), name='anons'),
     path('anime/<int:pk>/', views.AnimeDetailView.as_view(), name='anime-id'),
 ]
