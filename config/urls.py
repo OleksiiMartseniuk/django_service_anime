58b4f038a75f23804b2e6d66edf1ed371c301a72
martsenyk.l@gmail.com <6n8at3TUS7>
2022-07-16 18:01:10 +0300

    settings media

--- config/urls.py
+++ config/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
@@ -35,3 +37,5 @@ urlpatterns = [
     # app anime
     path('v1/', include('src.anime.urls')),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
