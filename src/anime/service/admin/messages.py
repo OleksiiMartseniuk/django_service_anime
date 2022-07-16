ddc3a71457209e6668b378f763c3d82462afb3e4
martsenyk.l@gmail.com <6n8at3TUS7>
2022-07-16 22:45:12 +0300

    edit DElETE_SCHEDULE_FORM

--- src/anime/service/admin/messages.py
+++ src/anime/service/admin/messages.py
@@ -15,8 +15,7 @@ ERROR_DATA_API = 'ApiAnimeVostClientAttributeError\n' \
 ERROR_NOT_DATA = 'NotDataError\nНет данных'
 SCHEDULE_FORM = 'Запись аниме расписания'
 ANONS_FORM = 'Запись аниме анонсов'
-DElETE_SCHEDULE_FORM = 'Удаления всех записей таблиц ' \
-                       '(Anime, ScreenImages, Genre)'
+DElETE_SCHEDULE_FORM = 'Удаления всех записей таблиц Anime'
 SCHEDULE_UPDATE_FORM = 'Обновить аниме расписания'
 ANONS_UPDATE_FORM = 'Обновить аниме анонсов'
 SERIES_FORM = 'Запись серий'
