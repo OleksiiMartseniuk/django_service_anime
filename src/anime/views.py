d9c3b8404a5217d12b8ef48143f4ad8f4b687baf
martsenyk.l@gmail.com <6n8at3TUS7>
2022-07-18 13:41:22 +0300

    delete AnimeAnonsListView, AnimeScheduleDayView and create  AnimeListView

--- src/anime/views.py
+++ src/anime/views.py
@@ -1,32 +1,21 @@
 from django.utils.decorators import method_decorator
 
-from rest_framework.response import Response
-from rest_framework.views import APIView
-from rest_framework import generics, status
+from rest_framework import generics
 
 from drf_yasg import openapi
 from drf_yasg.utils import swagger_auto_schema
 
-from .service.pagination import LargeResultsSetPagination
+from .service.pagination import (
+    LargeResultsSetPagination,
+    StandardResultsSetPagination
+)
 from .models import Anime, Series
 from .service import service
 from .serializers import (
     AnimeSerializers,
-    AnimeMinAnonsSerializers,
-    DaySerializers,
     AnimeMinSerializers,
     SeriesSerializers
 )
-
-
-class AnimeAnonsListView(generics.ListAPIView):
-    """
-    Вывод списка аниме анонсов
-    ---
-    """
-    queryset = Anime.objects.filter(anons=True).\
-        only('id', 'title', 'url_image_preview', 'url_image_preview_s')
-    serializer_class = AnimeMinAnonsSerializers
 
 
 class AnimeDetailView(generics.RetrieveAPIView):
@@ -36,32 +25,6 @@ class AnimeDetailView(generics.RetrieveAPIView):
     """
     queryset = Anime.objects.all()
     serializer_class = AnimeSerializers
-
-
-class AnimeScheduleDayView(APIView):
-    """
-    Вывод расписания по дню
-    ---
-    """
-    @swagger_auto_schema(request_body=DaySerializers,
-                         responses={200: AnimeMinSerializers(many=True)})
-    def post(self, request):
-        serializer_day = DaySerializers(data=request.data)
-        if serializer_day.is_valid():
-            anime_list = service.get_anime_list_day(serializer_day.data['day'])
-            serializer_anime = AnimeMinSerializers(
-                anime_list,
-                context={'request': request},
-                many=True
-            )
-            return Response(
-                serializer_anime.data,
-                status=status.HTTP_200_OK
-            )
-        return Response(
-            serializer_day.errors,
-            status=status.HTTP_400_BAD_REQUEST
-        )
 
 
 @method_decorator(
@@ -89,3 +52,15 @@ class AnimeSeriesListView(generics.ListAPIView):
         queryset = Series.objects.filter(id_anime=id_anime).\
             values('id', 'name', 'std', 'hd', 'number').order_by('number')
         return queryset
+
+
+class AnimeListView(generics.ListAPIView):
+    """
+    Вывод всех аниме
+    ---
+    """
+    queryset = Anime.objects.only('id', 'title', 'url_image_preview',
+                                  'url_image_preview_s', 'timer')
+    serializer_class = AnimeMinSerializers
+    filterset_class = service.AnimeFilter
+    pagination_class = StandardResultsSetPagination
