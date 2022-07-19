b03ee8c9e3c46e516dc6c7d3b4a53dfd91337625
martsenyk.l@gmail.com <6n8at3TUS7>
2022-07-19 12:58:48 +0300

    add task auto_update

--- src/anime/tasks.py
+++ src/anime/tasks.py
@@ -1,6 +1,12 @@
+import logging
+
 from config.celery import app
 
 from .service.service_vost import ServiceAnime
+from .models import Anime, Series
+
+
+logger = logging.getLogger('main')
 
 
 @app.task
@@ -22,3 +28,26 @@ def parser(action: str) -> None:
             ServiceAnime().series_update()
         case 'delete_series':
             ServiceAnime().delete_series()
+
+
+@app.task
+def auto_update():
+    """Авто обновления данных"""
+    # Обновления расписания
+    if Anime.objects.count():
+        ServiceAnime().anime_schedule_update()
+        logger.info('Обновления успешно [schedule]')
+    else:
+        logger.error('Не данных для обновления [schedule]')
+    # Обновления анонсов
+    if Anime.objects.filter(anons=True).count():
+        ServiceAnime().anime_anons_update()
+        logger.info('Обновления успешно [anons]')
+    else:
+        logger.error('Не данных для обновления [anons]')
+    # Обновления серий
+    if Series.objects.count():
+        ServiceAnime().series_update()
+        logger.info('Обновления успешно [series]')
+    else:
+        logger.error('Не данных для обновления [series]')
