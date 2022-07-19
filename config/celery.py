09dad77b3e29932117c7ed74c9777050f8f4b563
martsenyk.l@gmail.com <6n8at3TUS7>
2022-07-19 12:59:06 +0300

    add beat ['add-every-day-morning']

--- config/celery.py
+++ config/celery.py
@@ -1,6 +1,7 @@
 import os
 
 from celery import Celery
+from celery.schedules import crontab
 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
@@ -9,3 +10,10 @@ app = Celery('config')
 
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
+
+app.conf.beat_schedule = {
+    'add-every-day-morning': {
+        'task': 'src.anime.tasks.auto_update',
+        'schedule': crontab(minute=0, hour='4'),
+    }
+}
