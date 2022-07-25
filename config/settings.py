ad0d765056068ac988af237fe51ee82c449cd049
martsenyk.l@gmail.com <6n8at3TUS7>
2022-07-25 19:36:22 +0300

    add default permission

--- config/settings.py
+++ config/settings.py
@@ -177,7 +177,8 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': [
         'django_filters.rest_framework.DjangoFilterBackend'
     ],
-    'DEFAULT_RENDERER_CLASSES': ('rest_framework.renderers.JSONRenderer',)
+    'DEFAULT_RENDERER_CLASSES': ('rest_framework.renderers.JSONRenderer',),
+    'DEFAULT_PERMISSION_CLASSES': ['rest_framework.permissions.AllowAny'],
 }
 
 CORS_ALLOWED_ORIGINS = [
