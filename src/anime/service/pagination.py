bc9b2fa19bcada1dfa28ebcce68394abd6ec510a
martsenyk.l@gmail.com <6n8at3TUS7>
2022-07-25 19:42:10 +0300

    delete file

--- src/anime/service/pagination.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.pagination import PageNumberPagination
-
-
-class LargeResultsSetPagination(PageNumberPagination):
-    page_size = 50
-
-
-class StandardResultsSetPagination(PageNumberPagination):
-    page_size = 20
