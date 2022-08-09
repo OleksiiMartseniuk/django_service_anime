202bb7dea59a839c1ac7cddc0bb1b84d2c68b200
OleksiiMartseniuk <martsenyk.l@gmail.com>
2022-08-09 22:21:24 +0300

    edit fild read default[False]

--- src/anime/models.py
+++ src/anime/models.py
@@ -108,7 +108,7 @@ class BotCollBackMessage(models.Model):
     """Сообщения пользователя"""
     id_user = models.IntegerField('id пользователя')
     message = models.TextField()
-    read = models.BooleanField('Прочитано', default=True)
+    read = models.BooleanField('Прочитано', default=False)
     created = models.DateTimeField()
 
     def __str__(self):
