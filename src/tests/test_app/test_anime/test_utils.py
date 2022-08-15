aef97a007fc901220c1ee385bdf43df95669c895
OleksiiMartseniuk <martsenyk.l@gmail.com>
2022-08-15 13:41:48 +0300

    edit import

--- src/tests/test_app/test_anime/test_utils.py
+++ src/tests/test_app/test_anime/test_utils.py
@@ -2,20 +2,16 @@ import os
 from unittest import mock
 from rest_framework.test import APITestCase
 
-from src.anime.service.utils import (
-    get_number,
-    download_image,
-    delete_img_files
-)
+from src.anime.service import utils
 from src.anime.models import ScreenImages
 
 
 class TestUtils(APITestCase):
     def test_get_number(self):
-        result = get_number('123 test')
+        result = utils.get_number('123 test')
         self.assertEqual(result, 123)
 
-        result = get_number('test')
+        result = utils.get_number('test')
         self.assertEqual(result, None)
 
     @mock.patch('src.anime.service.utils.requests.get')
@@ -27,7 +23,7 @@ class TestUtils(APITestCase):
             b'\x47\x49\x46\x38\x39\x61\x01\x00\x01\x00\x80\x00\x00\x05\x04\
             x04\x00\x00\x00\x2c\x00\x00\x00\x00\x01\x00\x01\x00\x00\x02\x02\
             x44\x01\x00\x3b')
-        download_image(obj.images_s, obj.images)
+        utils.download_image(obj.images_s, obj.images)
         self.assertTrue(obj.images_s)
 
         file_path = 'media/screen_images/test.gif'
@@ -36,5 +32,5 @@ class TestUtils(APITestCase):
 
     @mock.patch('src.anime.service.utils.os.remove')
     def test_delete_img_files(self, mock_remove):
-        delete_img_files()
+        utils.delete_img_files()
         mock_remove.assert_called()
