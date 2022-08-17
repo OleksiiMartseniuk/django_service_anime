b06fd3df64ca20ffa61255d6550ffc08bd128889
OleksiiMartseniuk <martsenyk.l@gmail.com>
2022-08-17 15:34:10 +0300

    add exception  and update_indefinite_exit

--- src/anime/tasks.py
+++ src/anime/tasks.py
@@ -1,4 +1,5 @@
 import logging
+import requests
 
 from config.celery import app
 
@@ -11,46 +12,67 @@ logger = logging.getLogger('main')
 
 @app.task
 def parser(action: str) -> None:
-    match action:
-        case 'schedule':
-            ServiceAnime().anime_schedule()
-        case 'anons':
-            ServiceAnime().anime_anons()
-        case 'delete':
-            ServiceAnime().delete_table()
-        case 'schedule_update':
-            ServiceAnime().anime_schedule_update()
-        case 'anons_update':
-            ServiceAnime().anime_anons_update()
-        case 'series':
-            ServiceAnime().series()
-        case 'series_update':
-            ServiceAnime().series_update()
-        case 'delete_series':
-            ServiceAnime().delete_series()
+    try:
+        match action:
+            case 'schedule':
+                ServiceAnime().anime_schedule()
+            case 'anons':
+                ServiceAnime().anime_anons()
+            case 'delete':
+                ServiceAnime().delete_table()
+            case 'schedule_update':
+                ServiceAnime().anime_schedule_update()
+            case 'anons_update':
+                ServiceAnime().anime_anons_update()
+            case 'series':
+                ServiceAnime().series()
+            case 'series_update':
+                ServiceAnime().series_update()
+            case 'delete_series':
+                ServiceAnime().delete_series()
+            case 'update_indefinite_exit':
+                ServiceAnime().update_indefinite_exit()
+
+    except requests.exceptions.RequestException as exr:
+        logger.error(exr)
+        Statistics.objects.create(message=f'Произошла ошибка #{exr.__class__}')
+    except Exception as ex:
+        logger.error(ex)
+        Statistics.objects.create(message=f'Произошла ошибка #{ex.__class__}')
 
 
 @app.task
 def auto_update():
     """Авто обновления данных"""
-    # Обновления расписания
-    if Anime.objects.count():
-        ServiceAnime().anime_schedule_update()
-        logger.info('Обновления успешно [schedule]')
-    else:
-        logger.error('Не данных для обновления [schedule]')
-    # Обновления анонсов
-    if Anime.objects.filter(anons=True).count():
-        ServiceAnime().anime_anons_update()
-        logger.info('Обновления успешно [anons]')
-    else:
-        logger.error('Не данных для обновления [anons]')
-    # Обновления серий
-    if Series.objects.count():
-        ServiceAnime().series_update()
-        logger.info('Обновления успешно [series]')
-    else:
-        logger.error('Не данных для обновления [series]')
+    try:
+        # Обновления расписания
+        if Anime.objects.count():
+            ServiceAnime().anime_schedule_update()
+            logger.info('Обновления успешно [schedule]')
+        else:
+            logger.error('Не данных для обновления [schedule]')
+        # Обновления анонсов
+        if Anime.objects.filter(anons=True).count():
+            ServiceAnime().anime_anons_update()
+            logger.info('Обновления успешно [anons]')
+        else:
+            logger.error('Не данных для обновления [anons]')
+        # Обновления серий
+        if Series.objects.count():
+            ServiceAnime().series_update()
+            logger.info('Обновления успешно [series]')
+        else:
+            logger.error('Не данных для обновления [series]')
 
-    # Запись в статистику
-    Statistics.objects.create(message='Авто обновления выполнено')
+        # Обновления аниме с неопределенным сроком выхода
+        ServiceAnime().update_indefinite_exit()
+
+        # Запись в статистику
+        Statistics.objects.create(message='Авто обновления выполнено')
+
+    except requests.exceptions.RequestException as exr:
+        logger.error(exr)
+        Statistics.objects.create(message=f'Произошла ошибка #{exr.__class__}')
+    except Exception as ex:
+        logger.error(ex)
+        Statistics.objects.create(message=f'Произошла ошибка #{ex.__class__}')
