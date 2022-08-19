ff36931e5db71b570d56259f6306988215c9b052
OleksiiMartseniuk <martsenyk.l@gmail.com>
2022-08-19 21:41:45 +0300

    created view StatisticsBotView and BotCollBackMessageView

--- src/bot/views.py
+++ src/bot/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from .serializers import (
+    BotStatisticsSerializer,
+    BotCollBackMessageSerializer
+)
+
+
+class StatisticsBotView(generics.CreateAPIView):
+    """
+    Запись статистики бота
+    ---
+    """
+    serializer_class = BotStatisticsSerializer
+
+
+class BotCollBackMessageView(generics.CreateAPIView):
+    """
+    Запись сообщения пользователя бота
+    ---
+    """
+    serializer_class = BotCollBackMessageSerializer
