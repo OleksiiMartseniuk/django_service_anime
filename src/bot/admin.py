8725b05bac68837507098ffb09df3361412c0a57
OleksiiMartseniuk <martsenyk.l@gmail.com>
2022-08-19 20:51:50 +0300

    created models BotStatistics and BotCollBackMessage

--- src/bot/admin.py
+++ src/bot/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (
+    BotStatistics,
+    BotCollBackMessage
+)
+
+
+@admin.register(BotStatistics)
+class BotStatisticsAdmin(admin.ModelAdmin):
+    list_display = ('id_user', 'action', 'created')
+    list_filter = ('id_user', 'action', 'created')
+    search_fields = ('id_user', 'action')
+
+
+@admin.register(BotCollBackMessage)
+class BotCollBackMessageAdmin(admin.ModelAdmin):
+    list_display = ('id_user', 'created', 'read')
+    list_filter = ('id_user', 'created', 'read')
+    search_fields = ('id_user',)
--- src/bot/models.py
+++ src/bot/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class BotStatistics(models.Model):
+    """Статистика бота"""
+    id_user = models.IntegerField('id пользователя')
+    action = models.CharField(max_length=255)
+    message = models.CharField(max_length=255)
+    created = models.DateTimeField()
+
+    def __str__(self):
+        return f'Пользователь [{self.id_user}] действия [{self.action}]'
+
+
+class BotCollBackMessage(models.Model):
+    """Сообщения пользователя"""
+    id_user = models.IntegerField('id пользователя')
+    message = models.TextField()
+    read = models.BooleanField('Прочитано', default=False)
+    created = models.DateTimeField()
+
+    def __str__(self):
+        return f'Пользователь [{self.id_user}]'
