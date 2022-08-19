9bbee29e2a1a02d61d0b59a03459f588878f3470
OleksiiMartseniuk <martsenyk.l@gmail.com>
2022-08-19 21:42:16 +0300

    add path create-statistic and create-massage

--- src/bot/urls.py
+++ src/bot/urls.py
@@ -1,6 +1,16 @@
 from django.urls import path
+
 from . import views
 
 urlpatterns = [
-
+    path(
+        'statistic/',
+        views.StatisticsBotView.as_view(),
+        name='create-statistic'
+    ),
+    path(
+        'massage/',
+        views.BotCollBackMessageView.as_view(),
+        name='create-massage'
+    )
 ]
