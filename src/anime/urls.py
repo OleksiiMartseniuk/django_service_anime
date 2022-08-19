d1ef753d5c6aad04682dad228bf83534f0b2d832
OleksiiMartseniuk <martsenyk.l@gmail.com>
2022-08-19 21:40:32 +0300

    edit path

--- src/anime/urls.py
+++ src/anime/urls.py
@@ -5,6 +5,9 @@ urlpatterns = [
     path('anime/', views.AnimeListView.as_view(), name='anime'),
     path('anime/<int:pk>/', views.AnimeDetailView.as_view(), name='anime-id'),
     path('anime/genre/', views.GenreListView.as_view(), name='genre-list'),
-    path('anime/series/', views.AnimeSeriesListView.as_view(),
-         name='series'),
+    path(
+        'anime/series/',
+        views.AnimeSeriesListView.as_view(),
+        name='series'
+    ),
 ]
