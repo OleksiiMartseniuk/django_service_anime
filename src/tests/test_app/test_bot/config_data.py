7143289562840a173060b3c85db1da9d7a78d5e7
OleksiiMartseniuk <martsenyk.l@gmail.com>
2022-08-20 23:48:04 +0300

    add function create_anime and create_bot_id_image

--- src/tests/test_app/test_bot/config_data.py
+++ src/tests/test_app/test_bot/config_data.py
@@ -1,3 +1,7 @@
+from src.anime.models import Anime
+from src.bot.models import BotIdImage
+
+
 send_photo_data = {
     'result':
         {'photo':
@@ -8,3 +12,45 @@ send_photo_data = {
          ]
          }
 }
+
+
+def create_anime(
+        id_anime: int = 1,
+        title: str = 'title',
+        link: str = 'anime_data.link',
+        rating: int = 1,
+        votes: int = 1,
+        description: str = 'anime_data.description',
+        director: str = 'anime_data.director',
+        url_image_preview: str = 'url_image_preview',
+        url_image_preview_s: str = 'url_image_preview_s',
+        year: str = 'anime_data.year',
+        type: str = 'an',
+        anons: bool = True,
+        day_week: str = 'monday'
+):
+    return Anime.objects.create(
+        id_anime=id_anime,
+        title=title,
+        link=link,
+        rating=rating,
+        votes=votes,
+        description=description,
+        director=director,
+        url_image_preview=url_image_preview,
+        url_image_preview_s=url_image_preview_s,
+        year=year,
+        type=type,
+        anons=anons,
+        day_week=day_week
+    )
+
+
+def create_bot_id_image(
+        id_photo: str = 'TEST',
+        id_anime: int = 1
+):
+    return BotIdImage.objects.create(
+        id_photo=id_photo,
+        id_anime=id_anime
+    )
