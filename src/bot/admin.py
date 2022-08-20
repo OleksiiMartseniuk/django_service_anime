a9ba99125f1a9db1ba740cde0183e4b886c08b77
OleksiiMartseniuk <martsenyk.l@gmail.com>
2022-08-20 19:37:37 +0300

    register model BotIdImage

--- src/bot/admin.py
+++ src/bot/admin.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 
 from .models import (
     BotStatistics,
-    BotCollBackMessage
+    BotCollBackMessage,
+    BotIdImage
 )
 
 
@@ -20,3 +21,9 @@ class BotCollBackMessageAdmin(admin.ModelAdmin):
     list_filter = ('id_user', 'created', 'read')
     search_fields = ('id_user',)
     readonly_fields = ('created',)
+
+
+@admin.register(BotIdImage)
+class BotIdImageAdmin(admin.ModelAdmin):
+    list_display = ('id_anime',)
+    search_fields = ('id_anime',)
