9b10cedcb63613f7562fc8cdf7d15a330c00d683
OleksiiMartseniuk <martsenyk.l@gmail.com>
2022-08-21 16:23:55 +0300

    add task auto_write_img and Statistics

--- src/bot/tasks.py
+++ src/bot/tasks.py
@@ -2,6 +2,8 @@ import logging
 
 from config.celery import app
 
+from src.anime.models import Statistics
+
 from .services.bot import ServiceBot
 
 
@@ -15,4 +17,23 @@ def bot_form(action: str) -> None:
             case 'upload':
                 ServiceBot().write_images_telegram()
     except Exception as ex:
+        Statistics.objects.create(
+            message=f'Произошла ошибка[telegram-bot] #{ex.__class__}'
+        )
+        logger.error(ex)
+
+
+@app.task
+def auto_write_img() -> None:
+    """Авто запись картинок на сервис телеграм"""
+    try:
+        ServiceBot().write_images_telegram()
+        # Запись в статистику
+        Statistics.objects.create(
+            message='Авто запись картинок на сервис телеграм'
+        )
+    except Exception as ex:
+        Statistics.objects.create(
+            message=f'Произошла ошибка[telegram-bot] #{ex.__class__}'
+        )
         logger.error(ex)
