0194b0000fdeaa949b76c3f7d62f036464086c33
OleksiiMartseniuk <martsenyk.l@gmail.com>
2022-08-22 16:39:46 +0300

    edit test

--- src/tests/test_app/test_bot/test_utils.py
+++ src/tests/test_app/test_bot/test_utils.py
@@ -2,7 +2,6 @@ from rest_framework.test import APITestCase
 
 from unittest import mock
 
-from src.bot.models import BotIdImage
 from src.bot.services import utils
 
 from . import config_data
@@ -13,23 +12,29 @@ class TestUtils(APITestCase):
     def test_write_id_images(self, mock_send_photo):
         mock_send_photo.return_value = config_data.send_photo_data
 
-        self.assertEqual(BotIdImage.objects.count(), 0)
-        utils.write_id_images(1, 'path')
-        self.assertEqual(BotIdImage.objects.count(), 1)
+        anime = config_data.create_anime()
+        self.assertIsNone(anime.telegram_id_file)
+        utils.write_id_images(anime)
+        self.assertEqual(
+            anime.telegram_id_file,
+            config_data.send_photo_data['result']['photo'][-1]['file_id']
+        )
 
     @mock.patch('src.bot.services.utils.TelegramApiClient.send_photo')
     def test_write_id_images_data_none(self, mock_send_photo):
         mock_send_photo.return_value = None
 
-        self.assertEqual(BotIdImage.objects.count(), 0)
-        utils.write_id_images(1, 'path')
-        self.assertEqual(BotIdImage.objects.count(), 0)
+        anime = config_data.create_anime()
+        self.assertIsNone(anime.telegram_id_file)
+        utils.write_id_images(anime)
+        self.assertIsNone(anime.telegram_id_file)
 
     @mock.patch('src.bot.services.utils.logger', mock.Mock())
     @mock.patch('src.bot.services.utils.TelegramApiClient.send_photo')
     def test_write_id_images_except_key_error(self, mock_send_photo):
         mock_send_photo.return_value = {'status': False}
 
-        self.assertEqual(BotIdImage.objects.count(), 0)
-        utils.write_id_images(1, 'path')
-        self.assertEqual(BotIdImage.objects.count(), 0)
+        anime = config_data.create_anime()
+        self.assertIsNone(anime.telegram_id_file)
+        utils.write_id_images(anime)
+        self.assertIsNone(anime.telegram_id_file)
