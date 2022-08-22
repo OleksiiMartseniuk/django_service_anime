c4e4c564d9bb5d8afb86d44905315f2e9de0a317
OleksiiMartseniuk <martsenyk.l@gmail.com>
2022-08-22 16:39:10 +0300

    edit function write_id_images

--- src/bot/services/utils.py
+++ src/bot/services/utils.py
@@ -1,19 +1,20 @@
 import logging
 
-from src.bot.models import BotIdImage
+from src.anime.models import Anime
 
 from .telegram import TelegramApiClient
 
 logger = logging.getLogger('main')
 
 
-def write_id_images(id_anime: int, path_img: str) -> None:
+def write_id_images(anime: Anime) -> None:
     """Запись id photo telegram в базу"""
-    data = TelegramApiClient().send_photo(path_img)
+    data = TelegramApiClient().send_photo(anime.url_image_preview_s.path)
     if data:
         try:
             id_photo = data['result']['photo'][-1]['file_id']
-            BotIdImage.objects.create(id_photo=id_photo, id_anime=id_anime)
+            anime.telegram_id_file = id_photo
+            anime.save()
         except KeyError:
             logger.error(f'Нет ключа в словаре [{data}]')
         except Exception as ex:
