0698a5d3cdefa8dfc7840c4e3911e837b09c8de4
OleksiiMartseniuk <martsenyk.l@gmail.com>
2022-08-22 16:53:23 +0300

    add function create_anime_kwargs

--- src/tests/test_app/test_anime/config_data.py
+++ src/tests/test_app/test_anime/config_data.py
@@ -270,3 +270,37 @@ anime_schemas = Anime(
     genre='приключения, комедия',
     timer=0,
     type='ТВ')
+
+
+def create_anime_kwargs(
+        id_anime: int = 1,
+        title: str = 'title',
+        link: str = 'anime_data.link',
+        rating: int = 1,
+        votes: int = 1,
+        description: str = 'anime_data.description',
+        director: str = 'anime_data.director',
+        url_image_preview: str = 'url_image_preview',
+        url_image_preview_s: str = 'url_image_preview_s',
+        telegram_id_file: str = None,
+        year: str = 'anime_data.year',
+        type: str = 'an',
+        anons: bool = True,
+        day_week: str = 'monday'
+) -> models.Anime:
+    return models.Anime.objects.create(
+        id_anime=id_anime,
+        title=title,
+        link=link,
+        rating=rating,
+        votes=votes,
+        description=description,
+        director=director,
+        url_image_preview=url_image_preview,
+        url_image_preview_s=url_image_preview_s,
+        telegram_id_file=telegram_id_file,
+        year=year,
+        type=type,
+        anons=anons,
+        day_week=day_week
+    )
