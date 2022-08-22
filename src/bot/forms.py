bdcfdec0ff90b960cf8135a74722f2eda3a55b3a
OleksiiMartseniuk <martsenyk.l@gmail.com>
2022-08-22 16:25:15 +0300

    delete BotForm

--- src/bot/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-from src.base import messages
-
-
-class BotForm(forms.Form):
-    action = forms.ChoiceField(label='', widget=forms.Select, choices=[
-        ('upload', messages.UPLOAD_BOT_FORM),
-    ])
