b0b065ebeaab7cbd7dbf5461fa0c852ef136f278
OleksiiMartseniuk <martsenyk.l@gmail.com>
2022-08-23 21:20:57 +0300

    add count user

--- src/bot/admin.py
+++ src/bot/admin.py
@@ -1,6 +1,8 @@
 import logging
 
 from django.contrib import admin
+from django.template.response import TemplateResponse
+from django.urls import path
 
 from .models import BotStatistics, BotCollBackMessage
 
@@ -14,6 +16,22 @@ class BotStatisticsAdmin(admin.ModelAdmin):
     list_filter = ('id_user', 'action', 'created')
     search_fields = ('id_user', 'action')
     readonly_fields = ('created',)
+
+    def get_urls(self):
+        urls = super().get_urls()
+        my_urls = [
+            path('bot-info/', self.admin_site.admin_view(self.bot_info),
+                 name='bot-info'),
+        ]
+        return my_urls + urls
+
+    def bot_info(self, request):
+        """Информация о боте"""
+        context = dict(
+            self.admin_site.each_context(request),
+            count=BotStatistics.objects.values('id_user').distinct().count(),
+        )
+        return TemplateResponse(request, 'admin/bot.html', context)
 
 
 @admin.register(BotCollBackMessage)
