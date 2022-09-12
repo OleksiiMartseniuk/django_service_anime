611d074e51d1a4de83a5f5f8888a61cb095dbc38
Oleksii Martseniuk <martsenyk.l@gmail.com>
2022-09-12 19:09:12 +0300

    add view AddAnimeUserView

--- src/bot/views.py
+++ src/bot/views.py
@@ -1,11 +1,15 @@
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 from .serializers import (
     BotStatisticsSerializer,
     BotCollBackMessageSerializer,
-    BotUserCreateSerializer
+    BotUserCreateSerializer,
+    AddAnimeUserSerializer
 )
+
+from .services import utils
 
 
 class StatisticsBotView(generics.CreateAPIView):
@@ -26,10 +30,29 @@ class BotCollBackMessageView(generics.CreateAPIView):
     permission_classes = [IsAuthenticated]
 
 
-class BotUserCreate(generics.CreateAPIView):
+class BotUserCreateView(generics.CreateAPIView):
     """
     Создания пользователя
     ---
     """
     serializer_class = BotUserCreateSerializer
     permission_classes = [IsAuthenticated]
+
+
+class AddAnimeUserView(generics.GenericAPIView):
+    """
+    Добавить аниме в отслеживаемые пользователем
+    ---
+    """
+    permission_classes = [IsAuthenticated]
+    serializer_class = AddAnimeUserSerializer
+
+    def post(self, request):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            utils.add_anime(
+                serializer.data['anime_ids'],
+                serializer.data['user_id']
+            )
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
