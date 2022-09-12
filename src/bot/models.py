e38584046ae6d21b840ca1de73c2a7f746e1c7f3
Oleksii Martseniuk <martsenyk.l@gmail.com>
2022-09-12 16:18:50 +0300

    add model BotUser

--- src/bot/models.py
+++ src/bot/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from src.anime.models import Anime
+
 
 class BotStatistics(models.Model):
     """Статистика бота"""
@@ -21,3 +23,14 @@ class BotCollBackMessage(models.Model):
 
     def __str__(self):
         return f'Пользователь [{self.id_user}]'
+
+
+class BotUser(models.Model):
+    """Пользователь бота"""
+    username = models.CharField('Имя пользователя', max_length=255)
+    user_id = models.IntegerField('ID пользователя telegram')
+    chat_id = models.IntegerField('ChatID пользователя telegram')
+    anime = models.ManyToManyField(Anime, blank=True)
+
+    def __str__(self):
+        return self.username
