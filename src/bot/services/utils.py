5e3bfa1d2fc4481ecef6aff825c9462dbd662510
Oleksii Martseniuk <martsenyk.l@gmail.com>
2022-09-12 17:38:44 +0300

    add function add_anime

--- src/bot/services/utils.py
+++ src/bot/services/utils.py
@@ -1,8 +1,11 @@
 import logging
 
+from rest_framework.exceptions import ValidationError
+
 from src.anime.models import Anime
 
 from .telegram import TelegramApiClient
+from ..models import BotUser
 
 logger = logging.getLogger('main')
 
@@ -21,3 +24,19 @@ def write_id_images(anime: Anime) -> None:
             logger.error(ex)
     else:
         logger.error(f'Нет данных в переменой data [{anime.title}]')
+
+
+def add_anime(anime_ids: list[int], user_id: int) -> None:
+    """Добавить аниме в отслеживаемые"""
+    if Anime.objects.filter(id__in=anime_ids).exists():
+        anime_objs = Anime.objects.filter(id__in=anime_ids)
+    else:
+        logger.debug('Аниме не найдено')
+        raise ValidationError('Аниме не найдено', code=404)
+
+    if BotUser.objects.filter(user_id=user_id).exists():
+        user = BotUser.objects.get(user_id=user_id)
+        user.anime.add(*anime_objs)
+    else:
+        logger.debug('Пользователь не найде')
+        raise ValidationError('Пользователь не найде', code=404)
