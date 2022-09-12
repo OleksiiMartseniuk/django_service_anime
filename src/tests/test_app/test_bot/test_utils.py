cba2c08ee3778abc5d2a089167635f4c8e2db539
Oleksii Martseniuk <martsenyk.l@gmail.com>
2022-09-12 17:38:56 +0300

    add function add_anime test

--- src/tests/test_app/test_bot/test_utils.py
+++ src/tests/test_app/test_bot/test_utils.py
@@ -1,4 +1,5 @@
 from rest_framework.test import APITestCase
+from rest_framework.exceptions import ValidationError
 
 from unittest import mock
 
@@ -39,3 +40,23 @@ class TestUtils(APITestCase):
         self.assertIsNone(anime.telegram_id_file)
         utils.write_id_images(anime)
         self.assertIsNone(anime.telegram_id_file)
+
+    @mock.patch('src.bot.services.utils.logger', mock.Mock())
+    def test_add_anime(self):
+        anime = config_data.create_anime()
+        user = config_data.create_bot_user()
+        self.assertEqual(user.anime.count(), 0)
+        utils.add_anime([anime.id], user.user_id)
+        self.assertEqual(user.anime.count(), 1)
+
+    @mock.patch('src.bot.services.utils.logger', mock.Mock())
+    def test_add_anime_invalid_anime(self):
+        user = config_data.create_bot_user()
+        self.assertEqual(user.anime.count(), 0)
+        self.assertRaises(ValidationError, utils.add_anime, [1], user.user_id)
+        self.assertEqual(user.anime.count(), 0)
+
+    @mock.patch('src.bot.services.utils.logger', mock.Mock())
+    def test_add_anime_invalid_user(self):
+        anime = config_data.create_anime()
+        self.assertRaises(ValidationError, utils.add_anime, [anime.id], 1)
