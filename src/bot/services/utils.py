9b49172f82971132a8b633d7b99480a6423839f4
Oleksii Martseniuk <martsenyk.l@gmail.com>
2022-09-17 22:21:16 +0300

    remove function write_id_images and add_anime, add get_image

--- src/bot/services/utils.py
+++ src/bot/services/utils.py
@@ -3,49 +3,12 @@ import re
 
 from datetime import datetime
 
-from rest_framework.exceptions import ValidationError
-
 from src.anime.models import Anime
 
-from .telegram import TelegramApiClient
 from ..models import BotUser, BotUserAnimePeriodTask
 
 
 logger = logging.getLogger('main')
-
-
-def write_id_images(anime: Anime) -> None:
-    """Запись id photo telegram в базу"""
-    data = TelegramApiClient().send_photo(anime.url_image_preview_s.path)
-    if data:
-        try:
-            id_photo = data['result']['photo'][-1]['file_id']
-            anime.telegram_id_file = id_photo
-            anime.save()
-        except KeyError:
-            logger.error(f'Нет ключа в словаре [{data}]')
-        except Exception as ex:
-            logger.error(ex)
-    else:
-        logger.error(f'Нет данных в переменой data [{anime.title}]')
-
-
-def add_anime(anime_ids: list[int], user_id: int) -> None:
-    """Добавить аниме в отслеживаемые"""
-    if Anime.objects.filter(id__in=anime_ids).exists():
-        anime_objs = Anime.objects.filter(id__in=anime_ids)
-    else:
-        logger.error(f'Аниме не найдено [{anime_ids}]')
-        raise ValidationError('Аниме не найдено', code=404)
-
-    if BotUser.objects.filter(user_id=user_id).exists():
-        user = BotUser.objects.get(user_id=user_id)
-        # BotUserAnimePeriodTask.objects.create()
-        # изменить добавления
-        # user.anime.add(*anime_objs)
-    else:
-        logger.error(f'Пользователь не найде [{user_id}]')
-        raise ValidationError('Пользователь не найде', code=404)
 
 
 def get_cart(anime: Anime) -> str:
@@ -71,3 +34,8 @@ def get_link_mirror(link: str) -> str | None:
         return link
     result = re.sub(r'/animevost.org/', '/v2.vost.pw/', link)
     return result
+
+
+def get_image(url_image_preview: str, telegram_id_file: str | None) -> str:
+    """Получения доступной картинки"""
+    return telegram_id_file if telegram_id_file else url_image_preview
