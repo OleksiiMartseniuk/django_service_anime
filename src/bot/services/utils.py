6d8460d618e97fe67734fb449c7eb52f781c8d1f
Oleksii Martseniuk <martsenyk.l@gmail.com>
2022-09-17 21:57:18 +0300

    add function get_cart and get_link_mirror

--- src/bot/services/utils.py
+++ src/bot/services/utils.py
@@ -1,11 +1,15 @@
 import logging
+import re
+
+from datetime import datetime
 
 from rest_framework.exceptions import ValidationError
 
 from src.anime.models import Anime
 
 from .telegram import TelegramApiClient
-from ..models import BotUser
+from ..models import BotUser, BotUserAnimePeriodTask
+
 
 logger = logging.getLogger('main')
 
@@ -31,12 +35,39 @@ def add_anime(anime_ids: list[int], user_id: int) -> None:
     if Anime.objects.filter(id__in=anime_ids).exists():
         anime_objs = Anime.objects.filter(id__in=anime_ids)
     else:
-        logger.debug(f'Аниме не найдено [{anime_ids}]')
+        logger.error(f'Аниме не найдено [{anime_ids}]')
         raise ValidationError('Аниме не найдено', code=404)
 
     if BotUser.objects.filter(user_id=user_id).exists():
         user = BotUser.objects.get(user_id=user_id)
-        user.anime.add(*anime_objs)
+        # BotUserAnimePeriodTask.objects.create()
+        # изменить добавления
+        # user.anime.add(*anime_objs)
     else:
-        logger.debug(f'Пользователь не найде [{user_id}]')
+        logger.error(f'Пользователь не найде [{user_id}]')
         raise ValidationError('Пользователь не найде', code=404)
+
+
+def get_cart(anime: Anime) -> str:
+    """Вывод аниме"""
+    if anime.timer:
+        date = datetime.utcfromtimestamp(anime.timer)
+        date = date.strftime('%H:%M')
+    else:
+        date = 'В течении дня'
+    time = f'<b>Время выхода</b> 🕜️ ({date}) \n'
+    return f"<b>{anime.title.split('/')[0]}</b> \n\n" \
+           f"{time}" \
+           f"<b>Рейтинг</b> 📊 {anime.rating}\n" \
+           f"<b>Голоса</b> 🗳️ {anime.votes}\n\n" \
+           f"<a href='{anime.link}'>Смотреть на animevost.org</a>\n" \
+           f"<a href='{get_link_mirror(anime.link)}'>Зеркало v2.vost.pw</a>"
+
+
+def get_link_mirror(link: str) -> str | None:
+    """Генерация ссылки зеркала"""
+    if not link:
+        # Ссылки нет
+        return link
+    result = re.sub(r'/animevost.org/', '/v2.vost.pw/', link)
+    return result
