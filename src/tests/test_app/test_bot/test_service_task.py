cc35405941cb776087c1f75988d8962cc187d550
Oleksii Martseniuk <martsenyk.l@gmail.com>
2022-09-18 17:56:47 +0300

    add test create_crontab_schedule

---
+++ src/tests/test_app/test_bot/test_service_task.py
@@ -0,0 +1,37 @@
+from rest_framework.test import APITestCase
+
+from django_celery_beat.models import CrontabSchedule
+
+from src.bot.services import task
+
+
+class TestServiceTest(APITestCase):
+
+    def test_create_crontab_schedule(self):
+        self.assertEqual(CrontabSchedule.objects.count(), 0)
+        schedule = task.create_crontab_schedule(1663277581, 'thursday')
+        self.assertEqual(CrontabSchedule.objects.count(), 1)
+        self.assertEqual(schedule.hour, '21')
+        self.assertEqual(schedule.minute, '33')
+        self.assertEqual(schedule.day_of_week, 'thursday')
+
+    def test_create_crontab_schedule_default(self):
+        self.assertEqual(CrontabSchedule.objects.count(), 0)
+        schedule = task.create_crontab_schedule(0, 'thursday')
+        self.assertEqual(CrontabSchedule.objects.count(), 1)
+        self.assertEqual(schedule.hour, '10')
+        self.assertEqual(schedule.minute, '0')
+        self.assertEqual(schedule.day_of_week, 'thursday')
+
+    def test_create_crontab_schedule_get(self):
+        schedule = CrontabSchedule.objects.create(
+            minute='0',
+            hour='10',
+            day_of_week='thursday'
+        )
+        self.assertEqual(CrontabSchedule.objects.count(), 1)
+        schedule_test = task.create_crontab_schedule(0, 'thursday')
+        self.assertEqual(CrontabSchedule.objects.count(), 1)
+        self.assertEqual(schedule.hour, schedule_test.hour)
+        self.assertEqual(schedule.minute, schedule_test.minute)
+        self.assertEqual(schedule.day_of_week, schedule_test.day_of_week)
