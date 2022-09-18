7a58bd3c33a7648e4a1f0e9f7d17f5b0cd1ab4b7
Oleksii Martseniuk <martsenyk.l@gmail.com>
2022-09-19 01:08:16 +0300

    add exceptions not day

--- src/bot/services/task.py
+++ src/bot/services/task.py
@@ -1,14 +1,22 @@
 import json
+import logging
 
 from datetime import datetime
 
+from rest_framework.exceptions import ValidationError
 from django_celery_beat.models import CrontabSchedule, PeriodicTask
 
 from src.bot.models import BotUser
 
 
+logger = logging.getLogger('main')
+
+
 def create_crontab_schedule(time: int, day: str) -> CrontabSchedule:
     """Создания кроны"""
+    if not day:
+        logger.error('Названия дня недели отсутствует')
+        raise ValidationError('Названия дня недели отсутствует')
     if time:
         date = datetime.utcfromtimestamp(time)
         hour = date.hour
