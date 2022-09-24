dad710add26915160cb368a82172bab299feb614
Oleksii Martseniuk <martsenyk.l@gmail.com>
2022-09-24 19:30:10 +0300

    add view RemoveAnimeUserView

--- src/bot/views.py
+++ src/bot/views.py
@@ -6,7 +6,7 @@ from .serializers import (
     BotStatisticsSerializer,
     BotCollBackMessageSerializer,
     BotUserCreateSerializer,
-    AddAnimeUserSerializer
+    TrackedAnimeUserSerializer
 )
 
 from .services import service
@@ -45,12 +45,31 @@ class AddAnimeUserView(generics.GenericAPIView):
     ---
     """
     permission_classes = [IsAuthenticated]
-    serializer_class = AddAnimeUserSerializer
+    serializer_class = TrackedAnimeUserSerializer
 
     def post(self, request):
         serializer = self.get_serializer(data=request.data)
         if serializer.is_valid():
             service.add_anime(
+                serializer.data['anime_ids'],
+                serializer.data['user_id']
+            )
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
+
+
+class RemoveAnimeUserView(generics.GenericAPIView):
+    """
+    Удаления аниме с отслеживаемые пользователем
+    ---
+    """
+    permission_classes = [IsAuthenticated]
+    serializer_class = TrackedAnimeUserSerializer
+
+    def post(self, request):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            service.delate_anime(
                 serializer.data['anime_ids'],
                 serializer.data['user_id']
             )
