66dcf21f0136b77bd7247903317f660b4bc4a980
Oleksii Martseniuk <martsenyk.l@gmail.com>
2022-09-25 19:53:36 +0300

    add view GetAnimeUserView

--- src/bot/views.py
+++ src/bot/views.py
@@ -2,11 +2,15 @@ from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
+from drf_yasg.utils import swagger_auto_schema
+
 from .serializers import (
     BotStatisticsSerializer,
     BotCollBackMessageSerializer,
     BotUserCreateSerializer,
-    TrackedAnimeUserSerializer
+    TrackedAnimeUserSerializer,
+    TrackedUserSerializer,
+    AnimeSerializer
 )
 
 from .services import service
@@ -74,4 +78,25 @@ class RemoveAnimeUserView(generics.GenericAPIView):
                 serializer.data['user_id']
             )
             return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
+
+
+class GetAnimeUserView(generics.GenericAPIView):
+    """
+    Вывод аниме отслеживаемые пользователем
+    ---
+    """
+    permission_classes = [IsAuthenticated]
+    serializer_class = TrackedUserSerializer
+
+    @swagger_auto_schema(responses={200: AnimeSerializer(many=True)})
+    def post(self, request):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            anime_list = service.get_anime_tracked(
+                serializer.data['user_id'],
+                serializer.data['subscriber']
+            )
+            serializer_anime_list = AnimeSerializer(anime_list, many=True)
+            return Response(serializer_anime_list.data, status=201)
         return Response(serializer.errors, status=400)
