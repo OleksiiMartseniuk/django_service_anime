90a920bbf551487fa845d09ccaf06056d8709393
Oleksii Martseniuk <martsenyk.l@gmail.com>
2023-03-04 12:55:05 +0200

    add const ACTIONS_ADMIN

---
+++ src/anime/constants.py
@@ -0,0 +1,15 @@
+from src.base import messages
+
+
+ACTIONS_ADMIN = [
+    ('schedule', messages.SCHEDULE_FORM),
+    ('anons', messages.ANONS_FORM),
+    ('schedule_update', messages.SCHEDULE_UPDATE_FORM),
+    ('anons_update', messages.ANONS_UPDATE_FORM),
+    ('series', messages.SERIES_FORM),
+    ('series_update', messages.SERIES_UPDATE_FORM),
+    ('delete_series', messages.DElETE_SERIES_FORM),
+    ('update_indefinite_exit', messages.UPDATE_INDEFINITE_EXIT),
+    ('write_telegram', messages.WRITE_TELEGRAM_BOT_FORM),
+    ('full_update', messages.FULL_UPDATE),
+]
