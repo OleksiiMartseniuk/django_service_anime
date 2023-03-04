689f880942128d5ab0af98ce1009edd0eaa384fd
Oleksii Martseniuk <martsenyk.l@gmail.com>
2023-03-04 16:35:54 +0200

    fix signal

--- src/anime/signals.py
+++ src/anime/signals.py
@@ -6,7 +6,9 @@ from .models import AnimeSettings
 
 @receiver(pre_save, sender=AnimeSettings)
 def save_task_status(sender, instance: AnimeSettings, **kwargs):
-    current = instance
-    previous = AnimeSettings.get_solo()
-    if current.status_task != previous.status_task:
-        current.set_status_task()
+    # The first run of the object model does not contain AnimeSettings
+    if AnimeSettings.objects.all().count():
+        current = instance
+        previous = AnimeSettings.get_solo()
+        if current.status_task != previous.status_task:
+            current.set_status_task()
