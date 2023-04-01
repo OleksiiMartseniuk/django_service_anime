71f9402a3bd46db612b2ec8777c6722fcf5289c2
Oleksii Martseniuk <martsenyk.l@gmail.com>
2023-04-01 19:04:30 +0300

    remove test_delete_img_files and edit field images

--- src/tests/test_app/test_anime/test_utils.py
+++ src/tests/test_app/test_anime/test_utils.py
@@ -16,24 +16,20 @@ class TestUtils(APITestCase):
 
     @mock.patch('src.anime.service.utils.requests.get')
     def test_download_image(self, mock_get):
-        obj = ScreenImages.objects.create(images='http://test.gif')
-        self.assertFalse(obj.images_s)
+        obj = ScreenImages.objects.create()
+        self.assertFalse(obj.images)
         mock_get.return_value.status_code = 200
         mock_get.return_value.content = (
             b'\x47\x49\x46\x38\x39\x61\x01\x00\x01\x00\x80\x00\x00\x05\x04\
             x04\x00\x00\x00\x2c\x00\x00\x00\x00\x01\x00\x01\x00\x00\x02\x02\
             x44\x01\x00\x3b')
-        utils.download_image(obj.images_s, obj.images)
-        self.assertTrue(obj.images_s)
+        utils.download_image(obj.images, 'http://test.gif')
+        self.assertTrue(obj.images)
 
         file_path = 'media/screen_images/test.gif'
         if os.path.isfile(file_path):
             os.remove(file_path)
 
-    @mock.patch('src.anime.service.utils.os.remove')
-    def test_delete_img_files(self, mock_remove):
-        utils.delete_img_files()
-        mock_remove.assert_called()
 
     def test_get_link(self):
         link = 'https://animevost.org/tip/tv/2761-tunshi-xingkong.html'
