c9e48bd745ee823e5f0b5f43584f1d49b1e28660
Oleksii Martseniuk <martsenyk.l@gmail.com>
2023-04-08 19:34:39 +0300

    add function create schemas

--- src/base/animevost/schemas.py
+++ src/base/animevost/schemas.py
@@ -1,3 +1,5 @@
+import re
+
 from enum import Enum
 
 from pydantic import BaseModel
@@ -51,3 +53,48 @@ class AnimeData(Anime):
 
 class AnimeFull(AnimeData):
     anime_composed: list[AnimeData]
+
+
+def create_anime_series(data: dict) -> Series:
+    serial = data.get('hd') or data.get('std', '')
+    return Series(
+        name=data.get('name'),
+        serial=serial.split('/')[-1].split('.')[0],
+        preview=data.get('preview')
+    )
+
+
+def create_anime_schemas(base_url: str, data: dict) -> Anime:
+    if re.search(r'^/', data.get('urlImagePreview')):
+        url_image_preview = base_url + data.get('urlImagePreview')
+    else:
+        url_image_preview = data.get('urlImagePreview')
+
+    screen_image = []
+    for img in data.get('screenImage'):
+        if re.search(r'^/', img):
+            screen_image.append(base_url + img)
+        elif not img:
+            pass
+        else:
+            screen_image.append(img)
+
+    remove_list = ['\n', '\r', '<br>', '<br />']
+    description = data.get('description', '')
+    for sub in remove_list:
+        description = description.replace(sub, '')
+
+    return Anime(
+        id=data.get('id'),
+        title=data.get('title'),
+        screen_image=screen_image,
+        rating=data.get('rating'),
+        votes=data.get('votes'),
+        description=description,
+        director=data.get('director'),
+        url_image_preview=url_image_preview,
+        year=data.get('year'),
+        genre=data.get('genre'),
+        timer=data.get('timer'),
+        type=data.get('type')
+    )
