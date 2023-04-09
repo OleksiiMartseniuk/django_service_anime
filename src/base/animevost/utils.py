62005e1c91852eecbd5a259bb6cf9c9e9c60c4ec
Oleksii Martseniuk <martsenyk.l@gmail.com>
2023-04-09 11:16:34 +0300

    add decorators

---
+++ src/base/animevost/utils.py
@@ -0,0 +1,16 @@
+import logging
+
+from functools import wraps
+
+
+logger = logging.getLogger('db')
+
+
+def exception_check(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except Exception as ex:
+            logger.error(exc_info=ex)
+    return wrapper
