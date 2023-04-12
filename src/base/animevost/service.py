b5fa02b8d3a201e0ec7cdaa7561dd10121526800
Oleksii Martseniuk <martsenyk.l@gmail.com>
2023-04-12 19:38:31 +0300

    add get anime indefinite exit

--- src/base/animevost/service.py
+++ src/base/animevost/service.py
@@ -1,6 +1,6 @@
 from .api import ApiAnimeVostClient
 from .parser import ParserClient
-from .schemas import AnimeData, AnimeFull, AnimeMin, Series
+from .schemas import AnimeData, AnimeFull, AnimeMin, Anime, Series
 from .utils import exception_check
 
 
@@ -46,19 +46,21 @@ class ServiceAnimeVost:
     def get_data_anime_all(
             self,
             full: bool = False
-    ) -> dict[str, list[AnimeFull | None]]:
+    ) -> dict[str, list[AnimeFull]] | dict:
         anime_week = {}
         data_parser = ParserClient().get_schedule(full)
         for key, value in data_parser.items():
-            anime_week[key] = self._create_anime_full_list(value)
+            if value:
+                anime_week[key] = self._create_anime_full_list(value)
         return anime_week
 
     def get_data_anime_anons_all(
             self,
             full: bool = False
-    ) -> list[AnimeFull | None]:
+    ) -> list[AnimeFull | None] | None:
         data_parser = ParserClient().get_anons(full)
-        return self._create_anime_full_list(data_parser)
+        if data_parser:
+            return self._create_anime_full_list(data_parser)
 
     def get_anime_data(self, id: int, link: str) -> AnimeFull | None:
         """Get one anime"""
@@ -68,3 +70,14 @@ class ServiceAnimeVost:
     @exception_check
     def get_list_series(self, anime_id: int) -> list[Series] | None:
         return ApiAnimeVostClient().get_play_list(id=anime_id)
+
+    @exception_check
+    def get_anime_indefinite_exit(
+        self,
+        page: int = 1,
+        quantity: int = 30
+    ) -> list[Anime] | None:
+        return ApiAnimeVostClient().get_last_anime(
+            page=page,
+            quantity=quantity,
+        )
