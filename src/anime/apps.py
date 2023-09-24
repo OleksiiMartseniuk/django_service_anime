f4bb5b7d3db946b0560b89183b370d41ef9911b6
Olekseii Martseniuk <martsenyk.l@gmail.com>
2023-09-24 13:50:10 +0300

    delete signals

--- src/anime/apps.py
+++ src/anime/apps.py
@@ -4,6 +4,3 @@ from django.apps import AppConfig
 class AnimeConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'src.anime'
-
-    def ready(self) -> None:
-        import src.anime.signals
