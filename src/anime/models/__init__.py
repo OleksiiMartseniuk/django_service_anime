4624452f11c2e4f7fd9f122c31159fa4b5abdb18
Oleksii Martseniuk <martsenyk.l@gmail.com>
2024-01-06 11:27:28 +0200

    create models Person, Team, AniLibria

--- src/anime/models/__init__.py
+++ src/anime/models/__init__.py
@@ -2,3 +2,5 @@ from .animevost import AnimeVost
 from .genre import Genre
 from .screen_image import ScreenImages
 from .series import Series
+from .team import Person, Team
+from .ani_libria import AniLibria
