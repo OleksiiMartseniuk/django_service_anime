6e18c491bb132694a91da43f08b8cac0ca76804f
Oleksii Martseniuk <martsenyk.l@gmail.com>
2024-01-07 22:14:18 +0200

    update download image

--- src/anime/utils.py
+++ src/anime/utils.py
@@ -8,18 +8,20 @@ from django.db.models.fields.files import ImageFieldFile
 logger = logging.getLogger('db')
 
 
-def download_image(obj_image: ImageFieldFile, image_url: str) -> None:
-    responses = requests.get(image_url)
-    if responses.status_code == 200:
-        try:
+def download_image(obj_image: ImageFieldFile, image_url: str) -> bool:
+    try:
+        responses = requests.get(image_url)
+        if responses.status_code == 200:
             fp = BytesIO()
             fp.write(responses.content)
             file_name = image_url.split("/")[-1]
             obj_image.save(file_name, File(fp))
-            return None
-        except Exception as ex:
-            logger.error("Download image", exc_info=ex)
-    logger.error(
-        f'Status Code [download image] [{responses.status_code}] '
-        f'url[{image_url}]'
-    )
+            return True
+        else:
+            logger.error(
+                f'Status Code [download image] [{responses.status_code}] '
+                f'url[{image_url}]'
+            )
+    except Exception as ex:
+        logger.error("Download image", exc_info=ex)
+    return False
